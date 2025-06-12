e975d4e61a5fe304b61f6cf40d80ce54e599e8a0
Shake <massaro-giuseppe@outlook.com>
2025-06-12 09:55:21 +0200

    Correzione file

--- 06_Esercizio2_scanf.c
+++ 06_Esercizio2_scanf.c
@@ -2,25 +2,24 @@
 
 int main(){
 char nome_town[20];
-int valore_1;
-int valore_2;
-int valore_3;
-int media;
+float valore_1;
+float valore_2;
+float valore_3;
+float media;
 
 printf("Ciao, digita il nome della tua città: ");
-scanf("%s", &nome_town);
+scanf("%s", nome_town);
 
 printf("Perfetto, ora digita tre numeri decimali: \n");
-scanf("%d", &valore_1);
-scanf("%d", &valore_2);
-scanf("%d", &valore_3);
+scanf("%f", &valore_1);
+scanf("%f", &valore_2);
+scanf("%f", &valore_3);
 
 media = (valore_1 + valore_2 + valore_3)/3;
-
 if (media <= 2){
     printf("La media è troppo bassa, inserisci tre nuovi numeri");
 } else {
-    printf("Ciao, il nome della tua città è %s e la media dei tre numeri è %d\n", nome_town, media);
+    printf("Ciao, il nome della tua città è %s e la media dei tre numeri è %.2f\n", nome_town, media);
     }
 
 return 0;
